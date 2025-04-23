8c8d872e88bd38a6c00ad035205ef34934d58d54
qq1521575701 <136119411+qq1521575701@users.noreply.github.com>
2025-04-23 21:54:29 +0800

    Update main.py

--- syn/main.py
+++ syn/main.py
@@ -1,25 +1,19 @@
 import ipaddress
 import random
 
-# 定义多个IP段
-networks = []
+# 读取所有 IP 段
+with open('cn_ip_part.txt', 'r', encoding='utf-8') as file:
+    networks = [ipaddress.IPv4Network(line.strip()) for line in file]
 
-with open('cn_ip_part.txt','r',encoding='utf-8') as file:
-    for i in file:
-        networks.append(ipaddress.IPv4Network(i.strip()))
-
+# 随机抽取 3000 个 IP 段
 networks = random.sample(networks, 3000)
 
-
-with open('cn_syn_ip.txt','w',encoding='utf-8') as file:
-    # 遍历每个网络段
+with open('cn_syn_ip.txt', 'w', encoding='utf-8') as file:
     for network in networks:
-        a = 0
-        # 获取该网络段中的所有IP地址
-        ips = list(network.hosts())
-
-        for ip in ips:
-            a += 1
-            if a < 5000:
-                # 将IP写入文件并打印
-                file.write(f'{ip} {random.randint(20000,65535)}\n')
+        count = 0
+        # 使用迭代器而不是 list()，节省内存、加快速度
+        for ip in network.hosts():
+            file.write(f'{ip} {random.randint(20000, 65535)}\n')
+            count += 1
+            if count == 5000:
+                break
